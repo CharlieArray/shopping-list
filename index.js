53ecb1e420fcee228f66a320ffb211a4974aa0da
Charles Ybarra <59151304+CharlieArray@users.noreply.github.com>
2020-09-17 22:23:27 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -15,8 +15,16 @@ $(function() {
     $("#js-shopping-list-form").submit(function(event) {
         event.preventDefault();
         const userTextElement = $("#shopping-list-entry").val();
-        $(".shopping-list").append(                   
-            "<li><p>" + userTextElement + "</p></li>");
+        const textOutput = `<li><span class='shopping-item'>'userTextElement'</span>
+        <div class='shopping-item-controls'>
+          <button class='shopping-item-toggle'>
+           <span class='button-label'>check</span>
+         </button>
+          <button class='shopping-item-delete'>
+          <span class='button-label'>delete</span>
+         </button>
+        </div></li>`;
+        $(".shopping-list").append(textOutput);
     }); 
 });
 
